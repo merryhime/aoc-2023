b55653492a21284727ab8d53e1e009d4d59ac322
merry <git@mary.rs>
2023-12-05 13:54:07 +0000

    Day 05: Part 2

--- day-05/main.d
+++ day-05/main.d
@@ -93,6 +93,84 @@ long part1(Almanac a)
     return a.seeds.map!(s => seedToLocation(a, s)).minElement;
 }
 
+struct Range
+{
+    long start;
+    long size;
+};
+
+Range[] mapLookup(Range[] ins, MapEntry[] m)
+{
+    Range[] result;
+next_ins:
+    while (!ins.empty)
+    {
+        Range i = ins.front;
+        ins.popFront();
+
+        foreach (MapEntry e; m)
+        {
+            long i0 = i.start;
+            long i1 = i.start + i.size;
+            long e0 = e.src;
+            long e1 = e.src + e.size;
+
+            auto translate = (long x) => x - e.src + e.dest;
+
+            if (i1 <= e0)
+                continue;
+            if (e1 <= i0)
+                continue;
+            if (e0 <= i0 && i1 <= e1)
+            {
+                result ~= Range(translate(i0), i1 - i0);
+                continue next_ins;
+            }
+            if (i0 < e0 && e1 < i1)
+            {
+                ins ~= Range(i0, e0 - i0);
+                result ~= Range(translate(e0), e1 - e0);
+                ins ~= Range(e1, i1 - e1);
+                continue next_ins;
+            }
+            if (e0 <= i0 && e1 < i1)
+            {
+                result ~= Range(translate(i0), e1 - i0);
+                ins ~= Range(e1, i1 - e1);
+                continue next_ins;
+            }
+            if (i0 < e0 && i1 <= e1)
+            {
+                ins ~= Range(i0, e0 - i0);
+                result ~= Range(translate(e0), i1 - e0);
+                continue next_ins;
+            }
+            writeln(i, e);
+            assert(false);
+        }
+
+        result ~= i;
+    }
+    return result;
+}
+
+Range[] seedToLocation(Almanac a, Range[] seed)
+{
+    return seed.mapLookup(a.seed_to_soil)
+               .mapLookup(a.soil_to_fertilizer)
+               .mapLookup(a.fertilizer_to_water)
+               .mapLookup(a.water_to_light)
+               .mapLookup(a.light_to_temp)
+               .mapLookup(a.temp_to_humidity)
+               .mapLookup(a.humidity_to_location);
+}
+
+long part2(Almanac a)
+{
+    Range[] ins = iota(0, a.seeds.length, 2).map!(i => Range(a.seeds[i], a.seeds[i + 1])).array;
+    return seedToLocation(a, ins).map!(r => r.start).minElement;
+}
+
 void main()
 {
     const string example1 = `seeds: 79 14 55 13
@@ -131,4 +209,6 @@ humidity-to-location map:
 
     writeln(part1(parseAlmanac(example1)));
     writeln(part1(parseAlmanac(readText("input"))));
+    writeln(part2(parseAlmanac(example1)));
+    writeln(part2(parseAlmanac(readText("input"))));
 }
