3e19e54cd7aa313bff3418957657303297e3390e
merry <git@mary.rs>
2023-12-12 17:02:43 +0000

    Day 12: Part 2

--- day-12/main.d
+++ day-12/main.d
@@ -12,31 +12,16 @@ import std.math.algebraic;
 import std.int128;
 
 struct Row {
-    string s;
-    Int128 mask;
-    Int128 set;
+    char[] s;
     long[] expect;
-    long setCount;
-    long width;
+    long maxHashCount;
+    long startingHashCount;
 }
 
 Row parseRow(string str) {
-    Row result;
-    result.s = str;
     string[] s = str.split(' ');
-    foreach (dchar ch; s[0]) {
-        result.mask <<= 1;
-        result.set <<= 1;
-        switch (ch) {
-        case '.': break;
-        case '#': result.set |= 1; result.setCount++; break;
-        case '?': result.mask |= 1; break;
-        default: assert(false);
-        }
-    }
-    result.width = s[0].length;
-    result.expect = s[1].split(',').map!(a => parse!long(a)).array;
-    return result;
+    auto expect = s[1].split(',').map!(a => parse!long(a)).array;
+    return Row(s[0].dup, expect, expect.sum, s[0].count!(a => a == '#'));
 }
 
 Row[] parseRows(string str) {
@@ -44,90 +29,98 @@ Row[] parseRows(string str) {
 }
 
 Row[] expandRows(Row[] rs) {
-    return rs.map!((Row r) {
+    return rs.map!((r) {
         Row newr = r;
+        char[] s = r.s.dup;
+        long[] expect = r.expect.dup;
         for (int i = 0; i < 4; i++) {
-            newr.mask <<= 1;
-            newr.set <<= 1;
-            newr.mask |= 1;
-            newr.set |= 0;
-            newr.mask <<= r.width;
-            newr.set <<= r.width;
-            newr.mask |= r.mask;
-            newr.set |= r.set;
-            newr.expect ~= r.expect;
+            newr.s ~= '?';
+            newr.s ~= s;
+            newr.expect ~= expect;
         }
+        newr.maxHashCount *= 5;
+        newr.startingHashCount *= 5;
         return newr;
     }).array;
 }
 
-long[] bitSeq(Int128 set) {
-    long[] result = [0];
-    while (set) {
-        if (set >> 127) {
-            result[$ - 1]++;
-        } else if (result[$ - 1]) {
-            result ~= 0;
-        }
-        set <<= 1;
+struct State {
+    long i;
+    long hashCount;
+    long[] current = [0];
+};
+
+long[State] cache;
+
+long countValid(Row row, State state) {
+    if (state in cache) {
+        return cache[state];
     }
-    return result.filter!"a!=0".array;
-}
 
-Int128 pext(Int128 x, Int128 mask) {
-    Int128 res = 0uL;
-    for (Int128 bb = 1uL; mask != 0uL; bb += bb) {
-        if (x & mask & -mask) {
-            res |= bb;
+    if (state.current.length >= row.expect.length) {
+        if (state.current == row.expect) {
+            for (long i = state.i; i < row.s.length; i++) {
+                switch (row.s[i]) {
+                case '.': break;
+                case '?': break;
+                case '#': return 0;
+                default: assert(false);
+                }
+            }
+            return 1;
         }
-        mask &= (mask - 1);
+        if (state.current.length > row.expect.length) {
+            return 0;
+        }
     }
-    return res;
-}
 
-Int128 pdep(Int128 val, Int128 mask) {
-  Int128 res = 0uL;
-  for (Int128 bb = 1uL; mask; bb += bb) {
-    if (val & bb)
-      res |= mask & -mask;
-    mask &= mask - 1;
-  }
-  return res;
-}
-
-
-Int128 next(Int128 x, Int128 mask) {
-    x = pext(x, mask);
-    Int128 a = x & -x;
-    Int128 b = x +  a;
-    Int128 c = x & ~b;
-    if (c != 0uL) {
-        while (!(c & 1)) {
-            c >>= 1;
-        }
-        c >>= 1;
+    if (state.i >= row.s.length) {
+        return 0;
     }
-    x = b ^ c;
-    x = pdep(x, mask);
-    return x;
-}
 
-long countValid(Row row) {
-    write(row.s);
-    long count = 0;
-    Int128 x = row.set | pdep(Int128((1uL << (row.expect.sum - row.setCount)) - 1uL), row.mask);
-    do {
-        if (bitSeq(x) == row.expect) {
-            count++;
+    /+Row modRow(char ch) {
+        Row newr = row;
+        newr.s = row.s.dup;
+        newr.s[state.i] = ch;
+        return newr;
+    }+/
+    long addZero() {
+        if (state.current[$ - 1] != 0) {
+            if (state.current[$ - 1] != row.expect[state.current.length - 1])
+                return 0;
+            return cache[state] = countValid(row, State(state.i + 1, state.hashCount, state.current ~ 0));
         }
-        x = next(x, row.mask) | row.set;
-    } while (x != row.set);
-    writeln(" -> ", count);
-    return count;
+        return cache[state] = countValid(row, State(state.i + 1, state.hashCount, state.current));
+    }
+    long addOne(int incr = 0) {
+        long[] newC = state.current.dup;
+        newC[$ - 1]++;
+        if (newC[$ - 1] > row.expect[state.current.length - 1])
+            return 0;
+        if (state.hashCount + incr > row.maxHashCount)
+            return 0;
+        return cache[state] = countValid(row, State(state.i + 1, state.hashCount + incr, newC));
+    }
+
+    // writeln("walk: ", state.i, " ", row.s[state.i], " - ", row.s.text, " ", state.current, "=", state.hashCount);
+
+    switch (row.s[state.i]) {
+    case '.':
+        return addZero();
+    case '#':
+        return addOne();
+    case '?':
+        long result = 0;
+        result += addZero();
+        result += addOne(1);
+        return cache[state] = result;
+    default:
+        assert(false);
+    }
 }
 
 long sumValid(Row[] rows) {
-    return rows.map!(countValid).sum;
+    return rows.map!((r) { writeln("==> ", r.s.text, " ", r.expect); cache.clear; long c = countValid(r, State(0, r.startingHashCount, [0])); writeln("==> ", c); return c; }).sum;
 }
 
 void main() {
@@ -138,10 +131,8 @@ void main() {
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1`;
 
-    assert(parseRows(example1).map!(r => r.width).maxElement + 1 < 128 / 5);
-    assert(parseRows(readText("input")).map!(r => r.width).maxElement + 1 < 128 / 5);
-
     writeln(sumValid(parseRows(example1)));
     writeln(sumValid(parseRows(readText("input"))));
-    // writeln(sumValid(expandRows(parseRows(example1))));
+    writeln(sumValid(expandRows(parseRows(example1))));
+    writeln(sumValid(expandRows(parseRows(readText("input")))));
 }
